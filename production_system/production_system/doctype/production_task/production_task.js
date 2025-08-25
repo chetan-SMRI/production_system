4b17bc02b0f13ecd0ca23fc32dc7dc30af0a49d1
chetan-SMRI <chetan2nahar@gmail.com>
2025-08-25 08:53:41 +0200

    doctypes ready

---
+++ production_system/production_system/doctype/production_task/production_task.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, CHETAN NAHAR and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Production Task", {
+// 	refresh(frm) {
+
+// 	},
+// });
