16dc707cd1cbc04ee10d3d797b0e2fe6fa10dc78
Chetan Nahar <chetan2nahar@gmail.com>
2026-02-27 18:38:45 +0530

    prod settings

---
+++ production_system/production_system/doctype/production_settings/test_production_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, CHETAN NAHAR and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestProductionSettings(FrappeTestCase):
+	pass
